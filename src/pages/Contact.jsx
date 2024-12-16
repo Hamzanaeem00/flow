8bec232eb0d24d87b4cb3a0a7761dca54668342c
hamzanaeem00 <hamza@pakistanautomation.com.pk>
2024-12-16 17:24:58 +0500

    fixed styling issue

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import ContactForm from '../components/ContactForm'
 
 const Contact = () => {
   return (
-    <div className=' max-w-[80vw] mt-20 mb-10 mx-auto grid grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 gap-2'>
+    <div className=' max-w-[80vw] mt-20 mb-10 mx-auto grid grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 gap-2' id='contact'>
       <ContactSocialMedia />
       <ContactForm />
 
