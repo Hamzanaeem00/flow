ed405e10ba93da059f59314c9819f07bf8bad7e3
Hurairahdct <AbuHuraira@pakistanautomation.com.pk>
2024-12-16 19:05:46 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import './App.css'
-import Header from './components/Header'
-import About from './pages/About'
-import EmblaCarousel from './pages/BlogCaurosal'
-import Contact from './pages/Contact'
-import Faq from './pages/Faq'
-import Home from './pages/Home'
-import Portfolio from './pages/Portfolio'
-import Services from './pages/Services'
+import "./App.css";
+import Header from "./components/Header";
+import About from "./pages/About";
+import EmblaCarousel from "./pages/BlogCaurosal";
+import Contact from "./pages/Contact";
+import Faq from "./pages/Faq";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Services from "./pages/Services";
 
 function App() {
-
   return (
     <>
-      <div className='font-inter'>
+      <div className="font-inter">
         <Header />
-       <Home />
-       <About />
-       <Services />
-       <Portfolio />
-       <Faq />
-       <EmblaCarousel   />
-       <Contact />  
+        <Home />
+        <About />
+        <Services />
+        <Portfolio />
+        <Faq />
+        <EmblaCarousel />
+        <Contact />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
