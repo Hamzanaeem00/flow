3062b4aaf4715752ee205dca0b101d90e739e285
Hamzadct <hamza@pakistanautomation.com.pk>
2024-12-20 22:13:10 +0500

    fix for small screens

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,16 +7,14 @@ const Card = ({ stat, description, number, isLastCard }) => {
       {/* Card Container */}
       <div className="bg-black w-full max-w-[300px] h-[230px] rounded-3xl cursor-pointer transition-transform transform group-hover:scale-105 group-hover:shadow-2xl group-hover:shadow-cyan-500/50 duration-300">
         <div className="py-2 px-6 flex flex-col justify-between h-full relative">
-          {/* {isLastCard === true?  */}
-          {/* ( */}
+  
             <img
             src={isLastCard? cardhover : "" }
             maxWidth="180px"
             alt=""
-            className=' absolute top-[-90px] right-[0px] z-10'
+            className=' absolute top-[-90px] right-[0px] z-10 sm:hidden xs:hidden md:hidden'
           />
-          {/* ) */}
-          {/* : ""} */}
+
         
           <div className="space-y-1 mt-5 text-left">
             <p className="text-[40px] font-normal tracking-tight text-cyan-400">
